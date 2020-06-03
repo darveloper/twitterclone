ddc4ba24c4ffd8782bd462bb398b77aabe267ee1
Darveloper <darveloper@gmail.com>
2020-06-03 14:49:46 -0400

    twitter feed

--- twitter/views.py
+++ twitter/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from twitteruser.models import Account
+from operator import attrgetter
+from tweet.models import Tweet
 
 def home_view(request):
     context = {}
-    accounts = Account.objects.all()
-    context['accounts'] = accounts
-
+    tweets = Tweet.objects.all().order_by('-date_updated')
+    context['tweets'] = tweets
     return render(request, 'home.html', context)
